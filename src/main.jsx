8b84e3654d93751140c4bf916613a370f1efdbf7
OLá Mundo <anabelanams@gmail.com>
2025-01-30 22:13:08 -0400

    commit

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import Home from './components/Home.vue';
-
-const app = createApp(App);
-app.component('Home', Home);
-app.mount('#app');
